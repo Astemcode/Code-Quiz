b8f7e58c1faa28addaedee9dc0cd9cfe45e08092
Joivre Charles <astemcode@gmail.com>
2020-10-22 15:09:33 -0400

    File updates
    Updated the JS object and array. Added the blank display for questions into the CSS file

---
+++ script.js
@@ -0,0 +1,23 @@
+var quiz = [
+    {question:"JS Question 1",
+    choices:["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+    answer:'Answer 1'
+},
+
+{question:"JS Question 2",
+    choices:["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+    answer:'Answer 2'
+},
+
+{question:"JS Question 3",
+    choices:["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+    answer:'Answer 3'
+},
+
+{question:"JS Question 4",
+    choices:["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+    answer:'Answer 4'
+}
+
+] 
+

e1ff579f545029ff6c5fc59c52e6fbfa72e4f337
Joivre Charles <astemcode@gmail.com>
2020-10-24 20:11:53 -0400

    Update questions.js
    Added the questions for the quiz

--- questions.js
+++ questions.js
@@ -1,22 +1,32 @@
 var quiz = [
     {question:"What does DOM stand for in JavaScript?",
-    choices:["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    answer:'Answer 1'
+    choices:["Dissolved Organic Matter", "Department of Management", "Document Object Model", "Dirty Old Man"],
+    answer: "Document Object Model"
 },
 
-{question:"JS Question 2",
-    choices:["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    answer:'Answer 2'
+{question:"Which framework is designed for creating dynamic websites?",
+    choices:["Static Frameworks", "Responsive Frameworks", "Constant Frameworks", "Contemporary Frameworks"],
+    answer: "Responsive Frameworks"
 },
 
-{question:"JS Question 3",
-    choices:["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    answer:'Answer 3'
+{question:"What is the index position of the third character in a string?",
+    choices:["Index [5]", "Index [1]", "Index [2]", "Index [3]"],
+    answer: "Index [2]"
 },
 
-{question:"JS Question 4",
-    choices:["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    answer:'Answer 4'
+{question:"True or False you can declare a function inside of a function?",
+    choices:["False", "True", "Man I Dont Know", "Both answers are correct"],
+    answer: "True"
+},
+
+{question:"Which of the following is NOT a variable?",
+    choices:["String", "Number", "Media", "Boolean"],
+    answer: "Media"
+},
+
+{question:"Which is NOT a JavaScript library?",
+    choices:["Mootools", "jQuery", "React", "Atom"],
+    answer: "Atom"
 }
 
 ] 

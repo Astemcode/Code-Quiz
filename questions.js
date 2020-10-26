c87d3975dc3a6b3f0ceb17dc07760a605ad81fee
Joivre Charles <astemcode@gmail.com>
2020-10-25 22:25:16 -0400

    Timer
    Timer is now working and the on click events are functional with JS

--- questions.js
+++ questions.js
@@ -1,31 +1,31 @@
 var quiz = [
     {question:"What does DOM stand for in JavaScript?",
-    choices:["Dissolved Organic Matter", "Department of Management", "Document Object Model", "Dirty Old Man"],
+    options:["Dissolved Organic Matter", "Department of Management", "Document Object Model", "Dirty Old Man"],
     answer: "Document Object Model"
 },
 
 {question:"Which framework is designed for creating dynamic websites?",
-    choices:["Static Frameworks", "Responsive Frameworks", "Constant Frameworks", "Contemporary Frameworks"],
+    options:["Static Frameworks", "Responsive Frameworks", "Constant Frameworks", "Contemporary Frameworks"],
     answer: "Responsive Frameworks"
 },
 
 {question:"What is the index position of the third character in a string?",
-    choices:["Index [5]", "Index [1]", "Index [2]", "Index [3]"],
+    options:["Index [5]", "Index [1]", "Index [2]", "Index [3]"],
     answer: "Index [2]"
 },
 
 {question:"True or False you can declare a function inside of a function?",
-    choices:["False", "True", "Man I Dont Know", "Both answers are correct"],
+    options:["False", "True", "Man I Dont Know", "Both answers are correct"],
     answer: "True"
 },
 
 {question:"Which of the following is NOT a variable?",
-    choices:["String", "Number", "Media", "Boolean"],
+    options:["String", "Number", "Media", "Boolean"],
     answer: "Media"
 },
 
 {question:"Which is NOT a JavaScript library?",
-    choices:["Mootools", "jQuery", "React", "Atom"],
+    options:["Mootools", "jQuery", "React", "Atom"],
     answer: "Atom"
 }
 
--- script.js
+++ script.js
@@ -1,23 +1,55 @@
-var quiz = [
-    {question:"JS Question 1",
-    choices:["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    answer:'Answer 1'
-},
 
-{question:"JS Question 2",
-    choices:["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    answer:'Answer 2'
-},
+function start() {
 
-{question:"JS Question 3",
-    choices:["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    answer:'Answer 3'
-},
+    remainingTime = 70;
+    document.getElementById("remainingTime").innerHTML = remainingTime;
 
-{question:"JS Question 4",
-    choices:["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    answer:'Answer 4'
+    timer = setInterval(function() {
+        remainingTime--;
+        document.getElementById("remainingTime").innerHTML = remainingTime;
+        //proceed to end the game function when timer is below 0 at any time
+        if (remainingTime <= 0) {
+            clearInterval(timer);
+            endGame(); 
+        }
+    }, 1000);
+
+    next();
 }
 
-] 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+var quizQuestion = document.getElementById("quizSection");
+var possibleAnswers = document.getElementById("quizSection");
+var answer;
+
+
+
+function createQuestions() {
+    questionOrder++;
+    answer = quiz[questionOrder].answer
+
+    quizQuestion.textContent = quiz[questionOrder].question;
+    possibleAnswers.innerHTML = "";
+
+    var options = quiz[questionOrder].options;
+
+    for (var q = 0; q < options.length; q++) {
+        var nextOption = document.createElement("button");
+
+        nextOption.textContent = options[q]
+        answerBtn = possibleAnswers.appendChild(nextOption).setAttribute("class", "p-3 m-1 btn btn-light btn-block");
+    }
+}
